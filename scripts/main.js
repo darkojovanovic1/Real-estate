cd215b1c14194ab65502932d81a45da3813ce53d
darko <darkoj123456789@gmail.com>
2024-08-03 13:07:49 +0200

    Popravi sllider

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper', {
+
+        
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+        },
+    });
+});
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
