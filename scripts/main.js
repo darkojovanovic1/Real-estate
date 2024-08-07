078b8535bb0c3e356af0d8e1634f3e8910944910
darko <darkoj123456789@gmail.com>
2024-08-07 04:19:11 +0200

    Value done

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -13,10 +13,10 @@ window.addEventListener('scroll', scrollHeader)
 document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.swiper', {
 
-        
+
         slidesPerView: 1,
         spaceBetween: 32,
-        grabCursor:true,
+        grabCursor: true,
         centeredSlides: true,
         slidesPerView: 'auto',
         navigation: {
@@ -42,15 +42,35 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value-accordion-item');
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value-accordion-header')
 
-
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+        toggleItem(item)
+        if(openItem && openItem !== item){
+            toggleItem(openItem)
+        }
+    })
+})
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value-accordion-content')
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px';
+        item.classList.add('accordion-open')
+    }
+}
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
