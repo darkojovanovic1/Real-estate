716a7f0dc2975b01f655b5babf1a49e1ee48b289
darko <darkoj123456789@gmail.com>
2024-08-13 23:32:08 +0200

    gotov projekat

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,11 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
 const scrollHeader = () => {
     const header = document.getElementById('header')
     this.scrollY >= 50 ? header.classList.add('scroll-header')
         : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-
-/*=============== SWIPER POPULAR ===============*/
 document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.swiper', {
-
-
         slidesPerView: 1,
         spaceBetween: 32,
         grabCursor: true,
@@ -39,8 +32,6 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 });
-
-/*=============== VALUE ACCORDION ===============*/
 const accordionItems = document.querySelectorAll('.value-accordion-item');
 accordionItems.forEach((item) => {
     const accordionHeader = item.querySelector('.value-accordion-header')
@@ -63,13 +54,10 @@ const toggleItem = (item) => {
         item.classList.add('accordion-open')
     }
 }
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('section[id]')
-
     const scrollActive = () => {
         const scrollDown = window.scrollY
-
         sections.forEach(current => {
             const sectionHeight = current.offsetHeight,
                 sectionTop = current.offsetTop - 58,
@@ -85,48 +73,44 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     window.addEventListener('scroll', scrollActive)
 });
-
-
-
-/*=============== SHOW SCROLL UP ===============*/
-
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
         : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
-
-/*=============== DARK LIGHT THEME ===============*/
-
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
-
-// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
-
-// We validate if the user previously chose a topic
 if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
 }
-
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+document.addEventListener('DOMContentLoaded', () => {
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 400,
+        //reset: true
+    });
+    sr.reveal('.home-title, .popular-container, .subscribe-container, .footer-container');
+    sr.reveal('.home-description, .footer-info', {delay:500});
+    sr.reveal('.home-search', {delay:600});
+    sr.reveal('.home-value', {delay:700});
+    sr.reveal('.home-images', {delay:800, origin: 'bottom'});
+    sr.reveal('.home-value', {delay:700});
+    sr.reveal('.logos-img', {interval:100});
+    sr.reveal('.value-img, .contact-content', {origin:'left'});
+    sr.reveal('.value-content, .contact-img', {origin:'right'});
+});
